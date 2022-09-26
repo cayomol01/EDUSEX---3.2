b0069c45d916bb281a8bbb5b2fe4337fc6036e8d
cayomol01 <jacmg01@gmail.com>
2022-09-26 17:00:26 -0600

    jest

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,7 @@
-<<<<<<< HEAD
 /* eslint-disable linebreak-style */
 module.exports = {
   testEnvironment: 'jsdom',
   moduleNameMapper: {
     '^.+.(css|less|scss)$': 'babel-jest',
-=======
-module.exports = {
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '^.+\\.(css|less|scss)$': 'babel-jest',
->>>>>>> 030bf15629f11bc5c6ddb0d190fea90b33fa0fec
   },
 };
