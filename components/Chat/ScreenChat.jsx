74dc4672bdc5abc7ba9068627119e4c69fa06300
Estef072 <61289015+Estef072@users.noreply.github.com>
2022-09-26 10:50:54 -0600

    terminado creo

--- components/Chat/ScreenChat.jsx
+++ components/Chat/ScreenChat.jsx
@@ -1,36 +1,100 @@
-import React, { useCallback, useLayoutEffect, useState } from 'react';
-import { View,Text } from 'react-native';
-import { GiftedChat } from 'react-native-gifted-chat'
+import { View,Text,  StyleSheet} from 'react-native';
+import React, { useState, useCallback, useEffect } from 'react'
+import {Bubble, GiftedChat, Send} from 'react-native-gifted-chat';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+
 
 function ScreenChat({ navigation }){
-    useEffect(() => {
-        setMessages([
-          {
-            _id: 1,
-            text: 'Hello developer',
-            createdAt: new Date(),
-            user: {
-              _id: 2,
-              name: 'React Native',
-              avatar: 'https://placeimg.com/140/140/any',
-            },
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'UserName',
+        createdAt: new Date(),
+        user: {
+          _id: 1,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+      {
+        _id: 2,
+        text: 'Hello world',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
+  }, [])
+  const renderSend = (props) => {
+    return (
+      <Send {...props}>
+        <View>
+          <MaterialCommunityIcons
+            name="send-circle"
+            size={42}
+            color="#c2dfe3"
+          />
+        </View>
+      </Send>
+    );
+  };
+
+  const renderBubble = (props) => {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#c2dfe3',
           },
-        ])
-      }, [])
-    
-      const onSend = useCallback((messages = []) => {
-        setMessages(previousMessages => GiftedChat.append(previousMessages, messages))
-      }, [])
-    
+        }}
+        textStyle={{
+          right: {
+            color: 'black',
+          },
+        }}
+      />
+    );
+  };
+
+  const scrollToBottomComponent = () => {
     return(
-        <GiftedChat
+      <FontAwesome name='angle-double-down' size={22} color='black' />
+    );
+  }
+
+  return (
+    <GiftedChat
       messages={messages}
-      onSend={messages => onSend(messages)}
+      onSend={(messages) => onSend(messages)}
       user={{
         _id: 1,
       }}
+      renderBubble={renderBubble}
+      alwaysShowSend
+      renderSend={renderSend}
+      scrollToBottom
+      scrollToBottomComponent={scrollToBottomComponent}
     />
-    )
+  );
     }
 
 export default ScreenChat
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor:'black'
+  },
+});
