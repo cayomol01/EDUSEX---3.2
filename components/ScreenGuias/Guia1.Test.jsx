a008f6f7b509f544124653f0504b98ce5e7fff0d
cayomol01 <jacmg01@gmail.com>
2022-10-05 18:19:02 -0600

    jest-solved

--- components/ScreenGuias/Guia1.Test.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import {
-render, screen, fireEvent,
-} from "@testing-library/react"
-import userEvent from "@testing-library/user-event"
-import "@testing-library/jest-dom/extend-expect"
--- jest.config.js
+++
@@ -1,7 +0,0 @@
-/* eslint-disable linebreak-style */
-module.exports = {
-  testEnvironment: 'jsdom',
-  moduleNameMapper: {
-    '^.+.(css|less|scss)$': 'babel-jest',
-  },
-};
