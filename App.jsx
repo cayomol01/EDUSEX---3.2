a67785a33d170316f2da237032c6f1d47867d923
TheJPMZ <monzon.jpmz@gmail.com>
2022-10-06 21:01:42 -0600

    ♻ Slight Refactor

--- App.jsx
+++ App.jsx
@@ -3,12 +3,6 @@ import MainStack from './navigation/MainStack';
 
 export default function App() {
   return (
-    // <SafeAreaProvider>
-    //   <SafeAreaView style={styles.container}>
-    //     <Text>Open up App.js to start working on your app!</Text>
-    //     <StatusBar style="auto" />
-    //   </SafeAreaView>
-    // </SafeAreaProvider>
     <MainStack />
   );
 }
