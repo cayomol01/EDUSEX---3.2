8e9aed3af16b6fd7094a7db70340cc8228eece04
TheJPMZ <monzon.jpmz@gmail.com>
2022-10-08 20:15:37 -0600

    🔎 Pruebas Genero.test.jsx

--- components/genero/Genero.jsx
+++ components/genero/Genero.jsx
@@ -93,7 +93,7 @@ function Genero({ navigation }) {
   return (
     <>
       <TouchableOpacity
-        onPress={() => navigation.navigate('ScreenChat')}
+        onPress={() => navigation.navigate('Login')}
       >
         <View style={styles.containerE}>
           <Image style={styles.imagenE} source={lEIMG} />
